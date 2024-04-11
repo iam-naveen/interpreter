8fecd745e6a855ac82a3fc326086ca63766c913a
Naveen <imnaveenbharath@gmail.com>
2024-04-11 18:31:27 +0530

    feat: filename in arguments

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-  "github.com/iam-naveen/compiler/console"
+	"github.com/iam-naveen/compiler/lexer"
+	"github.com/iam-naveen/compiler/parser"
 )
 
 func main() {
-  console.Run( os.Stdin, os.Stdout )
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide the input file")
+		return
+	}
+	input, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Error reading the file")
+		return
+	}
+	_, channel := lexer.CreateLexer(input)
+	parser.RunParser(channel)
 }
